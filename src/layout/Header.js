1b554fba2811eb717ff3a16ffe749e8eaf397818
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2023-12-12 00:49:04 +0300

    pages created

---
+++ src/layout/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>Header</p>
+    </>
+  );
+}
---
+++ src/layout/PageContent.js
@@ -0,0 +1,8 @@
+export default function PageContent() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>PageContent</p>
+    </>
+  );
+}

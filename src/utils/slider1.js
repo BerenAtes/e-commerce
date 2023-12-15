7a82c190c45ac86b44eadd3c7f17c7b823e05912
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2023-12-15 14:08:37 +0300

    img-main updated

--- src/utils/slider1.js
+++ src/utils/slider1.js
@@ -11,7 +11,7 @@ export default function HomeSlider() {
       }}
     >
       <div className="relative aspect-[2/1] text-clr-light">
-        <img className="img-absolute" src={slider} alt="img" />
+        <img className="img-main" src={slider} alt="img" />
         <div className="relative container-small z-10 top-[29%]">
           <div className="font-bold w-[60%] flex flex-col items-start gap-y-[2.1875rem]">
             <span>SUMMER 2020</span>
@@ -26,7 +26,7 @@ export default function HomeSlider() {
         </div>
       </div>
       <div className="relative aspect-[2/1]">
-        <img className="img-absolute" src={slider2} alt="img" />
+        <img className="img-main" src={slider2} alt="img" />
       </div>
     </AwesomeSlider>
   );

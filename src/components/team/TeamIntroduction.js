ed5c6d77239557f27eb9d321c86aa7ea7ac4f1c2
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2023-12-20 21:22:17 +0300

    team introduction component added

---
+++ src/components/team/TeamIntroduction.js
@@ -0,0 +1,13 @@
+import PagesPath from "../../components/PagesPath";
+
+export default function TeamIntroduction() {
+  return (
+    <>
+      <div>
+        <h5>WHAT WE DO</h5>
+        <h1>Innovation tailored for you</h1>
+        <PagesPath />
+      </div>
+    </>
+  );
+}
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,8 +1,10 @@
+import TeamIntroduction from "../components/team/TeamIntroduction";
 import TeamUserCard from "../components/team/TeamUserCard";
 
 export default function Team() {
   return (
     <>
+      <TeamIntroduction />
       <TeamUserCard />
     </>
   );

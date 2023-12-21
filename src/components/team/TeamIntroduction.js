3b890d5e63edfc22c074d5cf399d505d0275e7a1
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2023-12-21 20:33:12 +0300

    team page stills and responsive added

--- src/components/team/TeamIntroduction.js
+++ src/components/team/TeamIntroduction.js
@@ -3,9 +3,11 @@ import PagesPath from "../../components/PagesPath";
 export default function TeamIntroduction() {
   return (
     <>
-      <div>
-        <h5>WHAT WE DO</h5>
-        <h1>Innovation tailored for you</h1>
+      <div className="flex flex-col items-center">
+        <h5 className="text-[16px] text-clr-second font-bold">WHAT WE DO</h5>
+        <h1 className="text-[58px] text-clr-dark font-bold text-center">
+          Innovation tailored for you
+        </h1>
         <PagesPath />
       </div>
     </>
--- src/components/team/TeamPhotos.js
+++ src/components/team/TeamPhotos.js
@@ -7,16 +7,40 @@ import teamphoto5 from "../../assets/team/teamphoto5.png";
 export default function TeamPhotos() {
   return (
     <>
-      <div>
-        <section>
-          <div>
-            <img src={teamphoto1} alt="img"></img>
+      <div className="flex items-start justify-center m-[1rem] p-[.3rem]">
+        <section className="flex flex-wrap">
+          <div className="w-full lg:w-1/2">
+            <img
+              src={teamphoto1}
+              alt="img"
+              className="w-full h-auto mb-[.3rem] lg:mb-0"
+            />
           </div>
-          <div>
-            <img src={teamphoto2} alt="img"></img>
-            <img src={teamphoto3} alt="img"></img>
-            <img src={teamphoto4} alt="img"></img>
-            <img src={teamphoto5} alt="img"></img>
+          <div className="w-full lg:w-1/2 flex flex-wrap mb-[1rem]">
+            <div className="w-full lg:w-1/2 p-[.3rem]">
+              <img
+                src={teamphoto2}
+                alt="img"
+                className="w-full h-auto mb-[2.2rem]"
+              />
+              <img
+                src={teamphoto3}
+                alt="img"
+                className="w-full h-auto mb-[2.2rem]"
+              />
+            </div>
+            <div className="w-full lg:w-1/2 mb-[2.2rem]">
+              <img
+                src={teamphoto4}
+                alt="img"
+                className="w-full h-auto mb-[1.8rem]"
+              />
+              <img
+                src={teamphoto5}
+                alt="img"
+                className="w-full h-auto mb-[2.2rem]"
+              />
+            </div>
           </div>
         </section>
       </div>

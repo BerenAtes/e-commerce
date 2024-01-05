99733d9c8d1d17feeb06a1aec460e94402753470
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2024-01-05 12:24:33 +0300

    userActions added

---
+++ src/store/actions/userAction.js
@@ -0,0 +1,35 @@
+import { AxiosInstance, renewAxiosInstance } from "../../components/Api";
+
+export const setUser = (user) => ({
+  type: "SET_USER",
+  payload: user,
+});
+
+export const login = (data) => {
+  return {
+    type: "LOGIN_SUCCESS",
+    payload: data,
+  };
+};
+
+export const userLogout = () => {
+  return {
+    type: "LOGOUT",
+  };
+};
+
+export const userLogin = (login) => {
+  return async (dispatch) => {
+    return AxiosInstance.post("/login", login)
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        dispatch(login(response.data));
+        renewAxiosInstance();
+        return response;
+      })
+      .catch((error) => {
+        dispatch({ type: "LOGIN_UNSUCCESS", payload: error });
+        throw error;
+      });
+  };
+};

439706dcbb0d9fad10ab62848c622da21a66b59c
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2024-01-05 11:57:26 +0300

    globalActions added

---
+++ src/store/actions/globalAction.js
@@ -0,0 +1,33 @@
+import { AxiosInstance } from "../../components/Api";
+
+export const setRoles = (roles) => ({
+  type: "SET_ROLES",
+  payload: roles,
+});
+
+export const setCategories = (categories) => ({
+  type: "SET_CATEGORIES",
+  payload: categories,
+});
+
+export const theme = (theme) => ({
+  type: "SET_THEME",
+  payload: theme,
+});
+
+export const setLanguage = (language) => ({
+  type: "SET_LANGUAGE",
+  payload: language,
+});
+
+export const fetchCategories = () => {
+  return (dispatch) => {
+    AxiosInstance.get("/categories")
+      .then((response) => {
+        dispatch(setCategories(response.data));
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
+  };
+};
